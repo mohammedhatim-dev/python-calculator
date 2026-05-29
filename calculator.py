86652a26395db60dcc2ddd52f581c32be97c332e
Mohammed Hatim <mohammed.hatim9990@gmail.com>
2026-05-29 07:00:20 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+fierst_number= input ("inter your fierst number : " )
+fierst_number =float(fierst_number)
+operator = input ("inter operator : ")
+scond_number =  input ("inter your scond number : " )
+scond_number =float(scond_number)
+if operator == "+":
+    print(fierst_number + scond_number )
+elif operator == "-":
+    print(fierst_number - scond_number )
+elif operator == "/":
+     print(fierst_number / scond_number )
+
+elif operator == "*":
+     print(fierst_number * scond_number )
+else : 
+     print ("wrong")
